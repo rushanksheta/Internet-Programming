aba2bf92db6d1b4e41f7905e92a4f2ab14920ae6
rushanksheta <65179672+rushanksheta@users.noreply.github.com>
2020-11-17 22:19:29 +0530

    .

--- Experiments/Experiment 1/script.js
+++ Experiments/Experiment 1/script.js
@@ -5,7 +5,7 @@ function setCookie(name, value, daysToLive) {
     if(typeof daysToLive === "number") {
         /* Sets the max-age attribute so that the cookie expires
         after the specified number of days */
-        cookie += "; max-age=" + (daysToLive*60*60);
+        cookie += "; max-age=" + (daysToLive*24*60*60*1000);
         
         document.cookie = cookie;
     }
